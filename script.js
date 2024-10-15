ea3f13649458c5528244e45a2f778afb4ce4448a
Reginaldo Santos <167729026+reginaldosanctos@users.noreply.github.com>
2024-10-15 13:43:18 -0300

    script.js

--- script.js
+++ script.js
@@ -4,20 +4,26 @@ function togglePasswordVisibility() {
     passwordInput.setAttribute('type', type);
 }
 
+// Lógica do formulário de login
 document.getElementById('loginForm').addEventListener('submit', function (e) {
     e.preventDefault();
     // Aqui você pode adicionar a lógica para login
-    alert('Login realizado com sucesso!');
+    alert('Login realizado com sucesso!');
 
     // Redireciona para a tela principal após o alerta
     window.location.href = 'principal.html';
+});
 
-document.querySelector('.menu-lateral').addEventListener('mouseenter', function () {
-        this.style.width = '250px';
-    });
-    
-document.querySelector('.menu-lateral').addEventListener('mouseleave', function () {
-        this.style.width = '80px';
-    });
-    
+// Adiciona eventos para o comportamento do menu lateral
+const menuLateral = document.querySelector('.menu-lateral');
+const conteudoPrincipal = document.querySelector('.conteudo-principal');
+
+menuLateral.addEventListener('mouseenter', function () {
+    this.style.width = '250px';
+    conteudoPrincipal.style.marginLeft = '250px'; // Ajusta a margem do conteúdo
+});
+
+menuLateral.addEventListener('mouseleave', function () {
+    this.style.width = '80px';
+    conteudoPrincipal.style.marginLeft = '80px'; // Restaura a margem do conteúdo
 });
